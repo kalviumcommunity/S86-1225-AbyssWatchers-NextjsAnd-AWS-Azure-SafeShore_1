7786daf7bca96fc9b17a46d12acba48be6dbb5e5
Cheonyeoun <vigneshpandiyan08@gmail.com>
2026-01-04 21:57:42 +0530

    update navbar included gravatar userprofile

--- safeshore/components/Navbar/src/Navbar.tsx
+++ safeshore/components/Navbar/src/Navbar.tsx
@@ -6,7 +6,7 @@ import MenuCard from "./menucard";
 export default function Navbar(){
     return(
         <>
-            <div className="bg-white p-6 sm:p-4 shadow-md">
+            <div className="bg-white/2 p-6 sm:p-4 shadow-md select-none">
                 <div className="container mx-auto flex justify-between items-center">
                     <Link href="/" className="flex items-center">
                         <Image
@@ -18,15 +18,15 @@ export default function Navbar(){
                         />
                         <span className="font-bold text-xl text-gray-800">SafeShore</span>
                     </Link>
-                    <div>
-                        <Link href="/about" className="text-gray-600 hover:text-gray-800 mx-4 px-4 py-2 text-gray-700 rounded-md hover:bg-gray-300">
+                    <div className="flex items-center gap-6">
+                        <Link href="/about" className="text-gray-600 hover:text-gray-800 px-4 py-2 text-gray-700 rounded-md hover:bg-gray-300">
                             About
                         </Link>
-                        <MenuCard name="Profile" />
 
-                        <Link href="/contact" className="text-gray-600 hover:text-gray-800 mx-4 px-4 py-2 text-gray-700 rounded-md hover:bg-gray-300">
+                        <Link href="/contact" className="text-gray-600 hover:text-gray-800 px-4 py-2 text-gray-700 rounded-md hover:bg-gray-300">
                             Contact
                         </Link>
+                        <MenuCard email="santhipandiyan03@gmail.com" />
                     </div>
                 </div>
             </div>
--- safeshore/components/Navbar/src/menucard.tsx
+++ safeshore/components/Navbar/src/menucard.tsx
@@ -6,13 +6,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import Image from "next/image"
+import md5 from "blueimp-md5"
+
+export default function MenuCard({email }: {email: string}) {
+
+  const hash = md5(email.trim().toLowerCase())
+  const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=200&d=identicon`
 
 
-export default function MenuCard({name}: {name: string}) {
   return (
     <DropdownMenu>
-  <DropdownMenuTrigger className="px-4 py-2 text-gray-700 rounded-md hover:bg-gray-300">
-    {name}
+  <DropdownMenuTrigger className="p-1 mr-2 text-gray-700 rounded-full hover:bg-gray-300 focus-visible:ring-0">
+    <Image src={gravatarUrl} alt="profile" width={40} height={40} className="rounded-full object-cover"/>
   </DropdownMenuTrigger>
   <DropdownMenuContent className="p-2 m-1">
     <DropdownMenuLabel>My Account</DropdownMenuLabel>
